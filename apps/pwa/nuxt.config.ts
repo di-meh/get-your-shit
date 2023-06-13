59230dd41746357f4e478e94cae465dd1d2fcc60
nikola panic <npanic@myges.fr>
2023-06-13 23:39:56 +0200

    setup PWA

--- apps/pwa/nuxt.config.ts
+++ apps/pwa/nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    modules: ['@vite-pwa/nuxt'],
+    pwa: {
+        manifest: {
+            name: 'Get Your Shit',
+            short_name: 'GYS',
+            description: 'Buy some dope shit online',
+            lang: 'en',
+        },
+        meta: {
+            name: 'Get Your Shit',
+        },
+        
+        devOptions: {
+            enabled: true,
+        },
+    }
+  })
