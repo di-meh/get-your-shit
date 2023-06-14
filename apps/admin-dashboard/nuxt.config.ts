bc91dea87f5999c8240f898c705b864e87f4284e
Mehdi Saber <m1d3s@protonmail.com>
2023-06-14 18:59:26 +0200

    💄✨ Added all UI + lint libraries necessary

--- apps/admin-dashboard/nuxt.config.ts
+++ apps/admin-dashboard/nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@formkit/nuxt',
+    '@pinia/nuxt',
+    '@nuxthq/ui',
+    '@nuxtjs/eslint-module'
+  ],
+  devtools: {
+    enabled: true
+  },
+  runtimeConfig: {
 
+  }
 })
---
+++ apps/admin-dashboard/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  plugins: [require('daisyui')],
+  content: [
+    './node_modules/@formkit/themes/dist/tailwindcss/genesis/index.cjs'
+  ]
+}
