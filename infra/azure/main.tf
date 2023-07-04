588cde4d265fdf5e0ef465a5090635c86eb94116
Mehdi Saber <m1d3s@protonmail.com>
2023-07-04 12:05:14 +0200

    Push images sur ACR WIP

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "3.63.0"
     }
   }
 }
@@ -32,7 +32,25 @@ resource "azurerm_container_registry" "acr" {
   }
 }
 
+resource "azurerm_kubernetes_cluster" "akc" {
+  location            = azurerm_resource_group.rg_main.location
+  name                = "gys-akc"
+  resource_group_name = azurerm_resource_group.rg_main.name
+  dns_prefix = "gys-akc"
 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "GYS"
+  }
+}
 
 
