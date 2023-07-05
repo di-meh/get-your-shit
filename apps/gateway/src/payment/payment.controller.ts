1fb7130e91030a610aeb5466a0436622681038c9
Mehdi Saber <m1d3s@protonmail.com>
2023-07-05 14:39:33 +0200

    WIP Payment

--- apps/gateway/src/payment/payment.controller.ts
+++ apps/gateway/src/payment/payment.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import {Public} from "../auth/auth.decorator";
 
 @Controller('payment')
 export class PaymentController {
@@ -8,6 +9,7 @@ export class PaymentController {
     @Inject('PAYMENT_SERVICE') private readonly client: ClientProxy,
   ) {}
 
+  @Public()
   @Get('ping')
   ping(): Observable<string> {
     return this.client.send('payment-service:ping', 'payment');
