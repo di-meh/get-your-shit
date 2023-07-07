f8e173a383ff807c63b42ffb9cdbffeb1d9bfbe2
Mehdi Saber <m1d3s@protonmail.com>
2023-07-07 11:04:44 +0200

    Fix Prisma on payment

--- services/payment-service/src/app.module.ts
+++ services/payment-service/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 import { StripeService } from './stripe.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService, StripeService],
+  providers: [AppService, StripeService],
 })
 export class AppModule {}
